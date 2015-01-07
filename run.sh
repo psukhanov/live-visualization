c6719fcab27882d6797b6f70a94c68527f2d9bbe
Paul <paulsukhanov@gmail.com>
2015-01-06 23:13:39 -0800

    added script to run servers

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wd=`pwd`
+cd ./Spacebrew-spacebrew-672a874/
+node node_server_forever.js &
+sleep 1
+cd ../cloudbrain-master/connectors/spacebrew/
+python example_spacebrew_server.py &
+#open "file://${wd}/Live Visualization/index.html?server=localhost"
