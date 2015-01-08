292c710e86364b0af925af0c4ab29df521d72243
Paul <paulsukhanov@gmail.com>
2015-01-08 10:42:00 -0800

    runs in foreground

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ cd ./Spacebrew-spacebrew-672a874/
 node node_server_forever.js &
 sleep 1
 cd ../cloudbrain-master/connectors/spacebrew/
-python example_spacebrew_server.py &
+python example_spacebrew_server.py 
 #open "file://${wd}/Live Visualization/index.html?server=localhost"
