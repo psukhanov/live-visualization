fcfb9590a9bf16be6166e85ffcfe56f1e2e322b0
Lincoln Nguyen <lincolnn07@gmail.com>
2015-01-21 08:49:24 -0800

    reformat divs and add countdown timer files

--- Live Visualization/main.js
+++ Live Visualization/main.js
@@ -1,26 +1,6 @@
 
 $(document).ready(function() {
 
-    // countdown timer
-    var countdown = $("#countdown").countdown360({
-        radius: 115,
-        seconds: 60,
-        label: ['sec', 'secs'],
-        fontColor: '#FFFFFF',
-        strokeStyle: '#398FBD',
-        fillStyle: '#6a8da7',
-        autostart: false,
-        onComplete: function () {
-          console.log('done');
-        }
-    });
-
-    countdown.start();
-
-    $('#countdown').click(function() {
-      countdown.extendTimer(15);
-    });
-
     // full page transitions
     $('#fullpage').fullpage({
         //Navigation
