09100c2a94999976db166fb09605f8b6e0ad2f7f
Lincoln Nguyen <lincolnn07@gmail.com>
2015-01-21 11:54:31 -0800

    added section ids to use fullpage plugin

---
+++ Live Visualization/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        menu: '#menu',
+        css3: true,
+        scrollingSpeed: 1000
+    });
+});
