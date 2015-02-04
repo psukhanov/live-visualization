aa828870ad37b9ae195061b3f9ac7925ac94390e
watsonix <watsonix@gmail.com>
2015-02-03 17:02:33 -0800

    run.bat now launches full screen and launches the sound

--- run.bat
+++ run.bat
@@ -5,5 +5,8 @@ start node node_server_forever.js
 timeout /t 1 /nobreak
 start node node_server_forever.js -p 9002 &
 timeout /t 1 /nobreak
+start "" "C:\Program Files (x86)\Windows Media Player\wmplayer.exe" "C:\Users\ExplorCogTech\Music\stream.wav"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk
+timeout /t 1 /nobreak
 cd ../cloudbrain-master/listeners/
 start python biodata_spacebrew_client.py 
